2a1594cb542024c0128ed9acb52441b2ac0bf88c
ZalaJaydipsinh <64979418+ZalaJaydipsinh@users.noreply.github.com>
2022-10-02 00:39:34 +0530

    lab 12 added

---
+++ lab12/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:lab12_tutorial1/pages/choose_location.dart';
+import 'package:lab12_tutorial1/pages/home.dart';
+import 'package:lab12_tutorial1/pages/loading.dart';
+
+//code by ce156 (jaydipsinh zala)
+
+void main() => runApp(
+      MaterialApp(
+          // home: Home(),
+          initialRoute: '/',
+          routes: {
+            '/': (context) => Loading(),
+            '/home': (context) => const Home(),
+            '/location': (context) => const ChooseLocation(),
+          }),
+    );
